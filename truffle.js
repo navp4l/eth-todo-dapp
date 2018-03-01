e3d7913ba284b4543d8ed872ec34dd6729a02d2e
Naveen Palaniswamy <8531695+palanisn@users.noreply.github.com>
2018-03-01 17:58:59 +1100

    Update config

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port : 7545,
       network_id : '*',
       gas : 5000000
+    },
+    ganache_cli : {
+      host : '127.0.0.1',
+      port : 8545,
+      network_id : '*',
+      gas : 5000000
     }
   }
 };
